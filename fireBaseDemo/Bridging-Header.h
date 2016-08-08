9a31ca779b5b3ed54861b973b819fa40cb4fafec
Ayaz Ahmad Tarar <Ayaz@UserXs-MacBook-Pro.local>
2016-08-08 12:53:14 +0500

    firebase signup and login

---
+++ fireBaseDemo/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  fireBaseDemo
+//
+//  Created by Ayaz Ahmad Tarar on 8/8/16.
+//  Copyright © 2016 tarar. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <GoogleSignIn/GoogleSignIn.h>
+#endif /* Bridging_Header_h */
--- fireBaseDemo/fireBaseDemo/ViewController.swift
+++ fireBaseDemo/fireBaseDemo/ViewController.swift
@@ -11,23 +11,73 @@ import Firebase
 class ViewController: UIViewController {
     
     
-    //let rootRef = FIRDatabase.database().referenceFromURL("https://demoproject-69aa0.firebaseio.com/")
-   //
+    @IBOutlet var email: UITextField!
+    
+    
+    @IBOutlet var password: UITextField!
+    
+    
+    
+    @IBAction func FacebookLogin(sender: AnyObject) {
+        
+         
+    }
+    
+    @IBAction func login(sender: AnyObject) {
+        FIRAuth.auth()?.signInWithEmail(email.text!, password: password.text!, completion: { (user, error) in
+            if(error != nil)
+            {
+            print("incorrect"+String(error))
+            }
+            else{
+               print("successfully loged in with user \(user?.uid)")
+            }
+        })
+       
+    }
+    
+    @IBAction func signup(sender: AnyObject) {
+        
+        
+        FIRAuth.auth()?.createUserWithEmail(email.text!, password:self.password.text!, completion: { (signupUser, signupError) in
+        
+            if signupError != nil
+            {
+           print("Error in creation"+String(signupError))
+            
+            }
+            else
+            {
+             
+        print("user created sucessfully")
+            
+            }
+        })
+        
+}
+    
+    
      
        override func viewDidLoad() {
         super.viewDidLoad()
         
         
-      var rootRef = FIRDatabase.database().reference()
+       var rootRef = FIRDatabase.database().reference()
+        //let rootRef = FIRDatabase.database().referenceFromURL("https://demoproject-69aa0.firebaseio.com/")
        //var rootRef = FIRDatabase.database().referenceFromURL("https://demoproject-69aa0.firebaseio.com/")
       //  rootRef.
         let message=[
         "text":"text2",
         "user":"user2"]
-        rootRef.childByAutoId().setValue(message)
+      //  rootRef.childByAutoId().setValue(message)
         rootRef.observeEventType(.ChildAdded) { (snapshot) in
             print(snapshot.value)
         }
+        
+        
+        
+        
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
